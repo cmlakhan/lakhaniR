ddac1b432781d6f1182de6d739694b05f1581780
Chirag Lakhani <chirag.lakhani@gmail.com>
2019-11-22 11:26:07 -0500

    initial commit

--- utilities.R
+++ utilities.R
@@ -3,17 +3,31 @@
 # Created on: 11/22/19
 
 
+cor_spearman_train <- function(x, data) {
+  cor(data[,x[1] ], data[,x[2]],method = 'spearman', use = 'pairwise.complete.obs')
+}
 
 
 
 run_correlation_df <- function(df,listVariables,numCores){
   pairs <- combn(ncol(df), 2, simplify=F)
 
-  cols_names_train <- data.frame(variable=names(train_numeric)) %>%
+  cols_names_train <- data.frame(variable=names(df)) %>%
     rownames_to_column('id') %>%
     mutate(id=as.numeric(id))
 
 
+  out <- mclapply(pairs, cor_spearman_train,mc.cores = numCores)
+
+  cor_train_list <- cbind(do.call(rbind, pairs), unlist(out)) %>%
+    data.frame(.) %>%
+    select(var1=X1,var2=X2,correlation_value=X3) %>%
+    inner_join(.,cols_names_train, by=c('var1'='id')) %>%
+    inner_join(.,cols_names_train, by=c('var2'='id')) %>%
+    rename(variable1=variable.x, variable2=variable.y, Field1=Field.x,
+           Field2=Field.y)
+
+  return(cor_train_list)
 
 
 }
